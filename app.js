4ad9240fcfb907d8b8cf6d95800da69f6e64be0d
Rayyan <rayhq3@gmail.com>
2026-06-18 14:24:43 +0530

    added routes

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+//External Module
 const express = require("express");
+//Local Module
+const userRouter = require("./routes/userRouter.js");
+const hostRouter = require("./routes/hostRouter.js");
 const app = express();
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use(hostRouter);
+
 const PORT = 3003;
-app.listen(Port, () => {
-  console.log(`Server is listening on Port:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on address: http://localhost:${PORT}`);
 });
---
+++ routes/hostRouter.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const hostRouter = express.Router();
+
+hostRouter.get("/host/add-home", (req, res, next) => {
+  res.send(`
+     <h1>Add your Home Here!</h1>
+     <form action="/host/add-home" method="POST">
+        <input type="text" name="houseName" placeholder="Enter the name of your house" />
+        <input type="submit"/>
+    `);
+});
+
+hostRouter.post("/host/add-home", (req, res, next) => {
+  console.log(req.body);
+  res.send(`
+    <h1>Home Registered successfully!</h1>
+    <a href = "/">Go to Home</a>
+    `);
+});
+
+module.exports = hostRouter;
